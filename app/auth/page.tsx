7573a770c915b1001f61f7d7a43ccafec091d64c
evan-walter <ewpw5678@gmail.com>
2023-07-11 14:02:05 -0700

    Create toggling functionality without `useCallback` for now

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,12 +3,41 @@
 import { useState } from 'react';
 import Input from '@/app/components/Input';
 
+const NEW_USER = {
+    userType: 'new',
+    mainPrompt: 'Register',
+    toggleButtonPrompt: 'Already have an account?',
+    toggleButtonText: 'Login',
+};
+
+const EXISTING_USER = {
+    userType: 'existing',
+    mainPrompt: 'Login',
+    toggleButtonPrompt: 'New here?',
+    toggleButtonText: 'Register',
+};
+
 export default function Auth() {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [variant, setVariant] = useState(EXISTING_USER);
 
     return (
         <div>
-            <h2>Sign in</h2>
+            <h2>{variant.mainPrompt}</h2>
+            {variant.userType === 'new' ? (
+                <Input
+                    label='Name'
+                    onChange={(e: any) => {
+                        setName(e.target.value);
+                    }}
+                    id='name'
+                    type='name'
+                    value={name}
+                />
+            ) : null}
             <Input
                 label='Email'
                 onChange={(e: any) => {
@@ -18,6 +47,28 @@ export default function Auth() {
                 type='email'
                 value={email}
             />
+            <Input
+                label='Password'
+                onChange={(e: any) => {
+                    setPassword(e.target.value);
+                }}
+                id='password'
+                type='password'
+                value={password}
+            />
+            <button className='rounded-full py-2 px-4 bg-blue-600'>
+                {variant.mainPrompt}
+            </button>
+            <p>{variant.toggleButtonPrompt}</p>
+            <button
+                onClick={() =>
+                    setVariant(
+                        variant.userType === 'new' ? EXISTING_USER : NEW_USER
+                    )
+                }
+            >
+                {variant.toggleButtonText}
+            </button>
         </div>
     );
 }
