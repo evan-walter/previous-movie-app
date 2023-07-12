dc2e44474bf94e0e10fdd30bce32ba0bdc43ad9f
evan-walter <ewpw5678@gmail.com>
2023-07-11 21:06:47 -0700

    Format indentation

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -4,73 +4,71 @@ import { useState, useCallback } from 'react';
 import Input from '@/app/components/Input';
 
 const NEW_USER = {
-    userType: 'new',
-    mainPrompt: 'Register',
-    toggleButtonPrompt: 'Already have an account?',
-    toggleButtonText: 'Login',
+	userType: 'new',
+	mainPrompt: 'Register',
+	toggleButtonPrompt: 'Already have an account?',
+	toggleButtonText: 'Login',
 };
 
 const EXISTING_USER = {
-    userType: 'existing',
-    mainPrompt: 'Login',
-    toggleButtonPrompt: 'New here?',
-    toggleButtonText: 'Register',
+	userType: 'existing',
+	mainPrompt: 'Login',
+	toggleButtonPrompt: 'New here?',
+	toggleButtonText: 'Register',
 };
 
 export default function Auth() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-    const [variant, setVariant] = useState(EXISTING_USER);
+	const [variant, setVariant] = useState(EXISTING_USER);
 
-    return (
-        <div>
-            <h2>{variant.mainPrompt}</h2>
-            {variant.userType === 'new' ? (
-                <Input
-                    label='Name'
-                    onChange={(e: any) => {
-                        setName(e.target.value);
-                    }}
-                    id='name'
-                    type='name'
-                    value={name}
-                />
-            ) : null}
-            <Input
-                label='Email'
-                onChange={(e: any) => {
-                    setEmail(e.target.value);
-                }}
-                id='email'
-                type='email'
-                value={email}
-            />
-            <Input
-                label='Password'
-                onChange={(e: any) => {
-                    setPassword(e.target.value);
-                }}
-                id='password'
-                type='password'
-                value={password}
-            />
-            <button className='rounded-full py-2 px-4 bg-blue-600'>
-                {variant.mainPrompt}
-            </button>
-            <p>{variant.toggleButtonPrompt}</p>
-            <button
-                onClick={useCallback(() => {
-                    setVariant((currVariant) =>
-                        currVariant.userType === 'new'
-                            ? EXISTING_USER
-                            : NEW_USER
-                    );
-                }, [])}
-            >
-                {variant.toggleButtonText}
-            </button>
-        </div>
-    );
+	return (
+		<div>
+			<h2>{variant.mainPrompt}</h2>
+			{variant.userType === 'new' ? (
+				<Input
+					label='Name'
+					onChange={(e: any) => {
+						setName(e.target.value);
+					}}
+					id='name'
+					type='name'
+					value={name}
+				/>
+			) : null}
+			<Input
+				label='Email'
+				onChange={(e: any) => {
+					setEmail(e.target.value);
+				}}
+				id='email'
+				type='email'
+				value={email}
+			/>
+			<Input
+				label='Password'
+				onChange={(e: any) => {
+					setPassword(e.target.value);
+				}}
+				id='password'
+				type='password'
+				value={password}
+			/>
+			<button className='rounded-full py-2 px-4 bg-blue-600'>
+				{variant.mainPrompt}
+			</button>
+			<p>{variant.toggleButtonPrompt}</p>
+			<button
+				onClick={useCallback(() => {
+					setVariant((currVariant) =>
+						currVariant.userType === 'new' ? EXISTING_USER : NEW_USER
+					);
+				}, [])}
+			>
+				{variant.toggleButtonText}
+			</button>
+		</div>
+	);
 }
--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,22 +1,28 @@
 interface InputProps {
-    id: string;
-    onChange: any;
-    value: string;
-    label: string;
-    type?: string;
+	id: string;
+	onChange: any;
+	value: string;
+	label: string;
+	type?: string;
 }
 
 export default function Input({
-    id,
-    onChange,
-    value,
-    label,
-    type,
+	id,
+	onChange,
+	value,
+	label,
+	type,
 }: InputProps) {
-    return (
-        <div>
-            <input className='border' id={id} value={value} type={type} onChange={onChange} />
-            <label htmlFor={id}>{label}</label>
-        </div>
-    );
+	return (
+		<div>
+			<input
+				className='border'
+				id={id}
+				value={value}
+				type={type}
+				onChange={onChange}
+			/>
+			<label htmlFor={id}>{label}</label>
+		</div>
+	);
 }
